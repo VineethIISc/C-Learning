b7fc0d79a4ab46c1bf5fe72b7ad6ebdea62b21bb
Dibakar Sen <Dibakar Sen@DESKTOP-Q6248TG>
2021-09-03 17:15:54 +0530

    Add project files.

---
+++ 00_Hello_World/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00_Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Print text into the terminal
+            Console.WriteLine("Hello World!");
+
+            // Request user to press a key
+            Console.ReadKey();
+        }
+    }
+}
